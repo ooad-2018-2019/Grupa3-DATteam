3e19e81e2b6fbda0c984d5825227d3d37aee58bb
dfermic2 <dfermic2@etf.unsa.ba>
2019-06-18 13:37:34 +0200

    added models for proizvod and racun

--- DAT-TEAM/Models/Proizvod.cs
+++ DAT-TEAM/Models/Proizvod.cs
@@ -1,8 +1,29 @@
 ﻿using System;
 
-public class Class1
+public class Proizvod
 {
-	public Class1()
+    public string Naziv { get; set; }
+    public int Id { get; }
+    public string Opis { get; set; }
+    public string Proizvodjac { get; set; }
+    public double Cijena { get; set; }
+    public DateTime rokTrajanja { get; set; }
+    public string lijekTip { get; set; }
+    public bool naRecept { get; set; }
+    public int kolicnia { get; set; }
+
+    public Proizvod(string naziv, int id, string opis, string proizvodjac, double cijena, DateTime rok, string tip, bool recept, int kol)
 	{
+        Naziv = naziv;
+        Id = id;
+        Opis = opis;
+        Proizvodjac = proizvodjac;
+        Cijana = cijena;
+        rokTrajanja = rok;
+        lijekTip = tip;
+        naRecept = recept;
+        kolicnia = kol;
 	}
+
+
 }
--- DAT-TEAM/Models/Racun.cs
+++ DAT-TEAM/Models/Racun.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 
-public class Class1
+public class Racun
 {
-	public Class1()
+    public int Id { get; }
+    public Radnik Narucilac { set; get; }
+    public DateTime datumKreiranja { set; get; }
+    public List<Proizvod> stavkeRacuna { set; get; }
+
+	public Racun()
 	{
 	}
 }
