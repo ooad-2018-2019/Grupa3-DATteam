0dadd22712d3c528b6199f9e14f78c5cfc30ffae
Amel <akarsic1@etf.unsa.ba>
2019-06-19 14:48:28 +0200

    dodao kontroler za racun i radnike

---
+++ DAT-TEAM/Controllers/RacunController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DATteam.Models;
+
+namespace DATteam.Controllers
+{
+    public class RacunController : Controller
+    {
+        private readonly DATContext _context;
+
+        public RacunController(DATContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Racun
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Racun.ToListAsync());
+        }
+
+        // GET: Racun/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var racun = await _context.Racun
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (racun == null)
+            {
+                return NotFound();
+            }
+
+            return View(racun);
+        }
+
+        // GET: Racun/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Racun/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,datumKreiranja")] Racun racun)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(racun);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(racun);
+        }
+
+        // GET: Racun/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var racun = await _context.Racun.FindAsync(id);
+            if (racun == null)
+            {
+                return NotFound();
+            }
+            return View(racun);
+        }
+
+        // POST: Racun/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,datumKreiranja")] Racun racun)
+        {
+            if (id != racun.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(racun);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RacunExists(racun.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(racun);
+        }
+
+        // GET: Racun/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var racun = await _context.Racun
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (racun == null)
+            {
+                return NotFound();
+            }
+
+            return View(racun);
+        }
+
+        // POST: Racun/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var racun = await _context.Racun.FindAsync(id);
+            _context.Racun.Remove(racun);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RacunExists(int id)
+        {
+            return _context.Racun.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DAT-TEAM/Controllers/RadniciController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DATteam.Models;
+
+namespace DATteam.Controllers
+{
+    public class RadniciController : Controller
+    {
+        private readonly DATContext _context;
+
+        public RadniciController(DATContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Radnici
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Radnik.ToListAsync());
+        }
+
+        // GET: Radnici/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var radnik = await _context.Radnik
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (radnik == null)
+            {
+                return NotFound();
+            }
+
+            return View(radnik);
+        }
+
+        // GET: Radnici/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Radnici/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id")] Radnik radnik)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(radnik);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(radnik);
+        }
+
+        // GET: Radnici/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var radnik = await _context.Radnik.FindAsync(id);
+            if (radnik == null)
+            {
+                return NotFound();
+            }
+            return View(radnik);
+        }
+
+        // POST: Radnici/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id")] Radnik radnik)
+        {
+            if (id != radnik.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(radnik);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RadnikExists(radnik.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(radnik);
+        }
+
+        // GET: Radnici/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var radnik = await _context.Radnik
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (radnik == null)
+            {
+                return NotFound();
+            }
+
+            return View(radnik);
+        }
+
+        // POST: Radnici/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var radnik = await _context.Radnik.FindAsync(id);
+            _context.Radnik.Remove(radnik);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RadnikExists(int id)
+        {
+            return _context.Radnik.Any(e => e.id == id);
+        }
+    }
+}
